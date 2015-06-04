ad9bc65ce387b5c942be1d6ff9885405de7eccb6
Arthur <arthur@calypso.i3s.unice.fr>
2015-06-04 09:47:23 +0200

    the places for the activities are now generated

--- Places.py
+++ Places.py
@@ -45,7 +45,7 @@ class Places:
             accumulator += ",0"
         return accumulator
 
-    def createMarkingPreferenceEntity(self, identifier, name, numberOfLevel):
+    def createMarkingPreferenceEntity(self, name, identifier, numberOfLevel):
         accumulator = ""
         accumulator += "<attribute name=\"MarkingList\" type=\"ColList\" identifier=\"" + str(self.iterator.next()) + "\" net=\"1\">\n<colList row_count=\"1\" col_count=\"2\" active_row=\"0\" active_col=\"0\">\n"
         accumulator += "<colList_head>\n<colList_colLabel>\n<![CDATA[Color/Predicate/Function]]>\n</colList_colLabel>\n"
@@ -59,12 +59,33 @@ class Places:
         accumulator += "show=\"1\" grparent=\"" + str(identifier) + "\" state=\"1\" pen=\"0,0,0\" brush=\"0,0,255\"/>\n"
         accumulator += "</graphics>\n</attribute>\n<graphics count=\"1\">\n"
         accumulator += "<graphic x=\"180.00\" y=\"200.00\" identifier=\"" + str(identifier) + "\" net=\"1\" "
-        accumulator += "show=\"1\" w=\"20.00\" h=\"20.00\" state=\"1\" pen=\"0,0,0\" brush=\"255,255,255\"/>"
+        accumulator += "show=\"1\" w=\"20.00\" h=\"20.00\" state=\"1\" pen=\"0,0,0\" brush=\"255,255,255\"/>\n"
         accumulator += "</graphics>\n</node>\n"
         return accumulator
 
     def makeEntityPlace(self, identifier, name, numberOfLevel):
         return self.createGraphicPreferences(identifier, name) + self.createMarkingPreferenceEntity(name, identifier, numberOfLevel)
+
+    def createMarkingPreferenceActivity(self, identifier, name):
+        accumulator = ""
+        accumulator += "<attribute name=\"MarkingList\" type=\"ColList\" identifier=\"" + str(self.iterator.next()) + "\" net=\"1\">\n<colList row_count=\"1\" col_count=\"2\" active_row=\"0\" active_col=\"0\">\n"
+        accumulator += "<colList_head>\n<colList_colLabel>\n<![CDATA[Color/Predicate/Function]]>\n</colList_colLabel>\n"
+        accumulator += "<colList_colLabel>\n<![CDATA[Marking]]>\n</colList_colLabel>\n</colList_head>\n<colList_body>\n"
+        accumulator += "<colList_row nr=\"0\">\n<colList_col nr=\"0\">\n<![CDATA[0]]>\n</colList_col>\n<colList_col nr=\"1\">\n"
+        accumulator += "<![CDATA[1]]>\n</colList_col>\n</colList_row>\n</colList_body>\n</colList>\n<graphics count=\"1\">\n"
+        accumulator += "<graphic xoff=\"34.00\" yoff=\"-19.00\" x=\"214.00\" y=\"181.00\" identifier=\"" + str(self.iterator.next()) + "\" net=\"1\" "
+        accumulator += "show=\"1\" grparent=\"" + str(identifier) + "\" state=\"1\" pen=\"0,0,0\" brush=\"255,255,255\"/>\n"
+        accumulator += "</graphics>\n</attribute>\n<attribute name=\"Colorset\" identifier=\"13666\" net=\"1\">\n<![CDATA[D]]>\n<graphics count=\"1\">\n"
+        accumulator += "<graphic yoff=\"-20.00\" x=\"180.00\" y=\"180.00\" identifier=\"" + str(self.iterator.next()) + "\" net=\"1\" "
+        accumulator += "show=\"1\" grparent=\"" + str(identifier) + "\" state=\"1\" pen=\"0,0,0\" brush=\"0,0,255\"/>\n"
+        accumulator += "</graphics>\n</attribute>\n<graphics count=\"1\">\n"
+        accumulator += "<graphic x=\"180.00\" y=\"200.00\" identifier=\"" + str(identifier) + "\" net=\"1\" "
+        accumulator += "show=\"1\" w=\"20.00\" h=\"20.00\" state=\"1\" pen=\"0,0,0\" brush=\"255,255,255\"/>"
+        accumulator += "</graphics>\n</node>\n"
+        return accumulator
+    
+    def makeActivityPlace(self, identifier, name):
+        return self.createGraphicPreferences(identifier, name) + self.createMarkingPreferenceActivity(identifier, name)
 
     def makeText(self):
         startPlaces = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<?xml-stylesheet type=\"text/xsl\" href=\"/xsl/spped2svg.xsl\"?>\n<Snoopy version=\"2\" revision=\"1.13\">\n<netclass name=\"Colored Extended Petri Net\"/>\n<nodeclasses count=\"4\">\n<nodeclass count=\"" + str(self.numberOfPlaces) + "\" name=\"Place\">\n"
@@ -72,6 +93,10 @@ class Places:
         accumulator = ""
         for i in (self.entityDefinition.split("\n")):
             numberOfLevel = i.split(":")[1].count(",") + 1
-            name = i.split(":")[0]
-            accumulator += self.makeEntityPlace(self.iterator.next(), name.rstrip().lsplit(), numberOfLevel)
+            name = i.split(":")[0].rstrip().lstrip()
+            accumulator += self.makeEntityPlace(self.iterator.next(), name, numberOfLevel)
+        for i in range(len(self.potentialDefinition.split("\n"))):
+            accumulator += self.makeActivityPlace(self.iterator.next(), "palpha" + str(i))
+        for i in range(len(self.mandatoryDefinition.split("\n"))):
+            accumulator += self.makeActivityPlace(self.iterator.next(), "pbeta" + str(i))
         return startPlaces + accumulator + endPlaces
